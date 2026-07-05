8d6908114509e27862497bea0c69c9642efef5ea
Slava Pavlutin <sl.pavlutin@gmail.com>
2026-07-05 12:06:42 +0300

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
 import { defineConfig } from 'vite';
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
-// import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  // todo: enable eslint in the final build
-  plugins: [react(), /* eslint() */, svgr({
-    include: "**/*.svg?react",
-  }), tsconfigPaths()],
+  base: '/enterprise-task-manager/',
+  plugins: [
+    react(),
+    svgr({
+      include: '**/*.svg?react',
+    }),
+    tsconfigPaths(),
+  ],
   resolve: {
     alias: {
       '@styles': path.resolve(__dirname, 'src/app/styles'),
+      '~': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
